05bc3a99181746ceea3760c196556404423cdd37
rzm <rzm_1205@163.com>
2020-09-11 17:23:32 +0800

    封装request

---
+++ service/baseUrl.js
@@ -0,0 +1,5 @@
+const baseURL = 'http://kf.ibenrobot.com'
+const timeout = 5000
+export {
+  baseURL,timeout
+}
--- service/network.js
+++ service/network.js
@@ -1,9 +1,14 @@
+import {
+  baseURL,
+  timeout
+} from './baseUrl.js'
 //封装的request请求
 export default function request(options) {
   return new Promise((resolve,reject) => {
     wx.request({
-      method: options.method,
-      url: options.url,
+      method: options.method || 'post',
+      url: baseURL + options.url,
+      timeout:timeout,
       data: options.data,
       header:{ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
       success: (res) =>{
